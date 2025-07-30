c6883493b6eb4c5f1f4ad1149f991874668c8c1f
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-07-30 20:25:52 -0300

    Projeto iniciado

---
+++ android/app/src/main/kotlin/com/example/alura_flutter_client_control/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.alura_flutter_client_control
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:alura_flutter_client_control/pages/client_types_page.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/clients_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Controle de clientes',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const ClientsPage(title: 'Clientes'),
+        '/tipos': (context) => const ClientTypesPage(title: 'Tipos de cliente'),
+      },
+    );
+  }
+}
